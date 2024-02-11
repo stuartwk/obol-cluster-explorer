9ca8bff38595295bcdd76c6b620b302c54c8a5e5
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2024-02-11 22:15:40 +0900

    debugging issues on vercel

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,15 +1,23 @@
 import type { DistributedValidatorClusters } from '$lib/types';
 
 export const load = async () => {
-	const response = await fetch('https://api.obol.tech/lock/network/mainnet');
-	const json = (await response.json()) as unknown as DistributedValidatorClusters;
+	try {
+		console.log('loading distributed-validators');
+		const response = await fetch('https://api.obol.tech/lock/network/mainnet');
+		const json = (await response.json()) as unknown as DistributedValidatorClusters;
 
-	// sort by cluster_definition.name
-	json.cluster_locks.sort((a, b) =>
-		a.cluster_definition.name.localeCompare(b.cluster_definition.name)
-	);
+		// sort by cluster_definition.name
+		json.cluster_locks.sort((a, b) =>
+			a.cluster_definition.name.localeCompare(b.cluster_definition.name)
+		);
 
-	return {
-		distributedValidatedClusters: json
-	};
+		return {
+			distributedValidatedClusters: json
+		};
+	} catch (error) {
+		console.error('Error loading distributed-validators', error);
+		return {
+			error
+		};
+	}
 };
