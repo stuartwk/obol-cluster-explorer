df67aaf8f7364ae130d0ebdf042356c5bd295b3e
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2024-02-12 00:11:30 +0900

    cleanup

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -6,33 +6,21 @@ export const config = {
 };
 
 export const load = async ({ fetch }: LoadEvent) => {
-	try {
-		console.log('loading distributed-validators');
-		const response = await fetch('https://api.obol.tech/lock/network/mainnet', {
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json'
-			}
-		});
+	const response = await fetch('https://api.obol.tech/lock/network/mainnet', {
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		}
+	});
 
-		console.log('response', response);
+	const json = (await response.json()) as unknown as DistributedValidatorClusters;
 
-		const json = (await response.json()) as unknown as DistributedValidatorClusters;
+	// sort by cluster_definition.name
+	json.cluster_locks.sort((a, b) =>
+		a.cluster_definition.name.localeCompare(b.cluster_definition.name)
+	);
 
-		console.log('json', json);
-
-		// sort by cluster_definition.name
-		json.cluster_locks.sort((a, b) =>
-			a.cluster_definition.name.localeCompare(b.cluster_definition.name)
-		);
-
-		return {
-			distributedValidatedClusters: json
-		};
-	} catch (error) {
-		console.error('Error loading distributed-validators', error);
-		return {
-			error
-		};
-	}
+	return {
+		distributedValidatedClusters: json
+	};
 };
